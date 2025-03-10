756da83948b06866a14863983500d05368b1f208
Evan-github1 <xiange28@polyprep.org>
2025-03-10 13:11:21 -0400

    removed unused import(s)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CamerasAndSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CamerasAndSensors.java
@@ -7,8 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-import java.util.ArrayList;
-
 @TeleOp
 public class CamerasAndSensors extends LinearOpMode {
 
